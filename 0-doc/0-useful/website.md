86561a954924b5358cefb89fb58871a70eae6f13
hbaltz <hugo.baltz@ensg.eu>
2016-05-24 17:04:58 -0400

    MAJ webiste

--- 0-doc/0-useful/website.md
+++ 0-doc/0-useful/website.md
@@ -5,3 +5,14 @@ https://cgcookie.com/2015/12/16/quick-start-guide-gear-vr-unity/
 ## Android : unity integration :
 
 http://www.goprogramming.space/unity-and-android-studio-integration/
+
+## Moverio Unity Plugin :
+
+https://github.com/Seantron/MoverioUnityPlugin
+
+## VR sample google :
+
+https://github.com/googlevr/gvr-android-sdk.git
+
+## Enable usb debbuging :
+http://forum.xda-developers.com/showthread.php?s=f39188c7bdce5b64a47bb814b03cf748&t=2748574&page=3
--- 3-src/android/UnityTest/app/src/main/java/com/example/hbaltz/unitytest/MainActivity.java
+++ 3-src/android/UnityTest/app/src/main/java/com/example/hbaltz/unitytest/MainActivity.java
@@ -1,20 +1,86 @@
 package com.example.hbaltz.unitytest;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity  extends Activity implements SurfaceHolder.Callback{
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    boolean previewing = false;
+    LayoutInflater controlInflater = null;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        Canvas testCan = new Canvas();
+        testCan.drawRGB(255,0,0);
+        testCan.scale(100 ,100);
+        surfaceView.draw(testCan);
+
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+    }
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        // TODO Auto-generated method stub
+        if(previewing){
+            camera.stopPreview();
+            previewing = false;
+        }
 
+        if (camera != null){
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+                camera.startPreview();
+                previewing = true;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+    // TODO Auto-generated method stub
+        camera = Camera.open();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    // TODO Auto-generated method stub
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+        previewing = false;
     }
 }
