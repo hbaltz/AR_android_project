0b8718139e38784ea238f3888c65467cc95e2d19
hbaltz <hugo.baltz@ensg.eu>
2016-05-25 16:33:33 -0400

    Add documents

--- 3-src/android/UnityTest/app/src/main/java/com/example/hbaltz/unitytest/MainActivity.java
+++ 3-src/android/UnityTest/app/src/main/java/com/example/hbaltz/unitytest/MainActivity.java
@@ -1,86 +1,25 @@
 package com.example.hbaltz.unitytest;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.graphics.Canvas;
-import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.graphics.PixelFormat;
-import android.hardware.Camera;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 import java.io.IOException;
 
-public class MainActivity  extends Activity implements SurfaceHolder.Callback{
+public class MainActivity  extends Activity {
 
-    Camera camera;
-    SurfaceView surfaceView;
-    SurfaceHolder surfaceHolder;
-    boolean previewing = false;
-    LayoutInflater controlInflater = null;
-
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.camera);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        super.setContentView(R.layout.activity_main);
 
-        getWindow().setFormat(PixelFormat.UNKNOWN);
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
-        Canvas testCan = new Canvas();
-        testCan.drawRGB(255,0,0);
-        testCan.scale(100 ,100);
-        surfaceView.draw(testCan);
+        //////////////////////////////////// Full screen : /////////////////////////////////////////
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        winParams.flags |= 0x80000000;
+        win.setAttributes(winParams);
 
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-    }
-
-
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                               int height) {
-        // TODO Auto-generated method stub
-        if(previewing){
-            camera.stopPreview();
-            previewing = false;
-        }
-
-        if (camera != null){
-            try {
-                camera.setPreviewDisplay(surfaceHolder);
-                camera.startPreview();
-                previewing = true;
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-    // TODO Auto-generated method stub
-        camera = Camera.open();
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-    // TODO Auto-generated method stub
-        camera.stopPreview();
-        camera.release();
-        camera = null;
-        previewing = false;
     }
 }
