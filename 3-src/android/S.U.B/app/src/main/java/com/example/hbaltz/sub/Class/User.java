e3c500d8f64309555af3d1ba63e4e36dd9a56d73
hbaltz <hugo.baltz@ensg.eu>
2016-06-01 11:42:05 -0400

    Created class Building and User

---
+++ 3-src/android/S.U.B/app/src/main/java/com/example/hbaltz/sub/Class/User.java
@@ -0,0 +1,110 @@
+package com.example.hbaltz.sub.Class;
+
+import com.esri.core.geometry.Geometry;
+import com.esri.core.geometry.GeometryEngine;
+import com.esri.core.geometry.Point;
+import com.esri.core.geometry.SpatialReference;
+import com.esri.core.geometry.Unit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hbaltz on 6/1/2016.
+ */
+public class User {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////// FIELDS: ///////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private Point Location;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////// CONSTRUCTORS: /////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public User(Point location){
+        this.Location = location;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////// GETTERS: //////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Point getLocation() {
+        return this.Location;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////// SETTERS: //////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void setLocation(Point location) {
+        this.Location = location;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////// FUNCTIONS: ////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Function which find the buildings below a distance of radius unit from the point
+     *
+     * @param geomen : A geometry engine (Esri)
+     * @param builds : The array of buildings
+     * @param spaRef : the spatial reference
+     * @param radius : The distance to consider a geometries like a NN (Nearest Neighbor)
+     * @param unit : The distance's unit
+     * @return an ArrayList of buldings which qre the nearest geometries to the point
+     */
+    public ArrayList<Building> nearestNeighbors(GeometryEngine geomen,
+                                                Building[] builds,
+                                                SpatialReference spaRef,
+                                                double radius, Unit unit){
+
+        ArrayList<Building> NN = new ArrayList<>();
+        int len_builds = builds.length;
+
+        Point loc = this.getLocation();
+
+        Geometry buffer = geomen.buffer(loc, spaRef, radius, unit);
+
+
+        for (int i=0; i<len_builds; i++){
+            if(builds[i]!=null) {
+                Geometry footprint = builds[i].getFootprint();
+                if (footprint != null) {
+                    if (geomen.intersects(buffer, footprint, spaRef)) {
+                        NN.add(builds[i]);
+                    }
+                }
+            }
+        }
+        return NN;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Function which calculate the distance between a point and all the buildings in an array
+     *
+     * @param geomen : A geometry engine (Esri)
+     * @param builds : The arrayList of buildings
+     * @param spaRef : The spatial reference
+     * @return an array of double which are the distance between the point and the buildings
+     */
+    public double[] distanceToBuilds(GeometryEngine geomen,
+                                          ArrayList<Building> builds,
+                                          SpatialReference spaRef){
+        int len_builds = builds.size();
+        double[] distances = new double[len_builds];
+
+        Point loc = this.getLocation();
+
+        for (int i=0; i<len_builds; i++){
+            distances[i] = geomen.distance(loc, builds.get(i).getFootprint(), spaRef);
+        }
+
+        return distances;
+    }
+}
