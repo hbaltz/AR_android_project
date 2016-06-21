c9f372818b451fbe99868859f94c2941ffdeb1cf
hbaltz <hugo.baltz@ensg.eu>
2016-06-21 16:29:11 -0400

    Tried to displayed at the rigth place footprints

--- 3-src/android/S.U.B/app/src/main/java/com/example/hbaltz/sub/Class/User.java
+++ 3-src/android/S.U.B/app/src/main/java/com/example/hbaltz/sub/Class/User.java
@@ -223,13 +223,13 @@ public class User {
         double dx = locPoi.getX() - locUsr.getX();
         double dy = locPoi.getY() - locUsr.getY();
         double dz = locPoi.getZ() - locUsr.getZ();
-        if(dz==0) dz = 1.8; // <-- PB found
+        if(dz==0) dz = 5;
 
         double pitch;
 
         pitch = Math.atan2(dy, Math.sqrt((Math.pow(dx,2))+(Math.pow(dz,2))));
         pitch = Math.toDegrees(pitch);
 
-        return pitch+45; // degrees
+        return pitch; // degrees
     }
 }
