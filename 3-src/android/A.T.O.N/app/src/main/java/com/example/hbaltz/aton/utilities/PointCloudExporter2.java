b279bc569e90be601e91f5315990a414a929efd7
hbaltz <hugo.baltz@ensg.eu>
2016-07-14 16:11:11 -0400

    Export works !!!!!

--- 3-src/android/A.T.O.N/app/src/main/java/com/example/hbaltz/aton/utilities/PointCloudExporter2.java
+++ 3-src/android/A.T.O.N/app/src/main/java/com/example/hbaltz/aton/utilities/PointCloudExporter2.java
@@ -10,9 +10,13 @@ import android.widget.Toast;
 import com.example.hbaltz.aton.R;
 import com.example.hbaltz.aton.renderer.PointCollection;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.FloatBuffer;
 import java.text.Format;
@@ -48,7 +52,7 @@ public class PointCloudExporter2 {
             Format formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm", Locale.FRENCH);
 
             String fileName = "pointcloud-" + formatter.format(new Date()) + ".xyz";
-            File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/pointclouds");
+            File f = new File(context.getFilesDir() + "");
             if (!f.exists()) {
                 f.mkdirs();
                 Log.d("created", "yeah");
@@ -72,10 +76,51 @@ public class PointCloudExporter2 {
                     }
                 }
                 os.close();
+
+                Log.d("Works?",""+ readFromFile(fileName).length());
+
             } catch (IOException e) {
                 Log.e("Creation", "File not creates");
             }
             return null;
         }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Read the information in the file
+     *
+     * @param file: the file that we want to read
+     * @return a string that contains all the information int the file
+     */
+    private String readFromFile(String file) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(file);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 }
