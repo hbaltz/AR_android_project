5790747e0770b5f85f14ac69e1b3d47db5d72e63
hbaltz <hugo.baltz@ensg.eu>
2016-07-15 11:19:23 -0400

    Keeped one pointExporter

--- 3-src/android/A.T.O.N/app/src/main/java/com/example/hbaltz/aton/utilities/PointCloudExporter.java
+++ 3-src/android/A.T.O.N/app/src/main/java/com/example/hbaltz/aton/utilities/PointCloudExporter.java
@@ -1,14 +1,12 @@
 package com.example.hbaltz.aton.utilities;
 
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.example.hbaltz.aton.MainActivity;
 import com.example.hbaltz.aton.R;
 import com.example.hbaltz.aton.renderer.PointCollection;
 
@@ -23,19 +21,19 @@ import java.io.OutputStream;
 import java.nio.FloatBuffer;
 import java.text.Format;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
 
 public class PointCloudExporter {
-
     private final Context context;
     private final PointCollection pointCollection;
+    private String filePath;
 
     public PointCloudExporter(Context context, PointCollection pointCollection) {
         this.context = context;
         this.pointCollection = pointCollection;
+
     }
 
 
@@ -47,136 +45,45 @@ public class PointCloudExporter {
 
         @Override
         protected Void doInBackground(PointCollection... params) {
-
             if (params.length == 0) {
                 return null;
             }
             PointCollection pointCollection = params[0];
             Format formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm", Locale.FRENCH);
 
+            String fileName = "pointcloud-" + formatter.format(new Date()) + ".xyz";
+            File f = new File(context.getFilesDir() + "");
+            if (!f.exists()) {
+                f.mkdirs();
+                Log.d("created", "yeah");
+            }
+            final File file = new File(f, fileName);
+            filePath = file.getPath();
 
-            String fileName = "pointcloud-" + formatter.format(new Date()) + ".txt";
-            File f = new File(context.getFilesDir() , fileName);
-
-            Log.d("filename", fileName);
-
-            boolean created = createFile(f);
-
-            if(created) {
-
+            Log.d("fPath", filePath);
+            try {
+                OutputStream os = new FileOutputStream(file);
                 int size = pointCollection.getCount();
                 FloatBuffer floatBuffer = pointCollection.getBuffer();
                 floatBuffer.rewind();
                 int progressCounter = 0;
-
                 for (int i = 0; i < size; i++) {
                     String row = String.valueOf(floatBuffer.get()) + " " + String.valueOf(floatBuffer.get()) + " " + String.valueOf(floatBuffer.get()) + "\n";
-
-                    addInfoToFile(f,row);
-
+                    os.write(row.getBytes());
                     progressCounter++;
                     if (progressCounter % (int) ((double) size / 200.0) == 0) {
                         publishProgress(progressCounter);
                     }
                 }
+                os.close();
 
                 Log.d("Works?",""+ readFromFile(fileName).length());
 
-            } else {
+            } catch (IOException e) {
                 Log.e("Creation", "File not creates");
             }
-
             return null;
-
         }
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /**
-     * Creates a file
-     *
-     * @param file: the file that we want to create
-     * @return true if the file has been created, false if not
-     */
-    private boolean createFile(File file){
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
-
-        return false;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /**
-     * Adds the string info to the File file
-     *
-     * @param file: the file that we want to modify
-     * @param info: the string that we want ot add to the file
-     * @return true if the file has been modified, false if not
-     */
-    private boolean addInfoToFile(File file, String info){
-
-        FileOutputStream writer = null;
-        try {
-            writer = context.openFileOutput(file.getName(), Context.MODE_APPEND);
-
-            Log.d("fle", file.toString());
-
-            Log.d("content",writer.toString());
-
-            writer.write(info.getBytes());
-            writer.flush();
-            writer.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return true;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /**
-     * Adds the list of strings data to the File file
-     *
-     * @param file: the file that we want to modify
-     * @param data: the list of strings that we want ot add to the file
-     * @return true if the file has been modified, false if not
-     */
-    private boolean addDataToFile(File file, ArrayList<String> data){
-
-        FileOutputStream writer = null;
-        try {
-            writer = context.openFileOutput(file.getName(), Context.MODE_APPEND);
-
-            Log.d("fle", file.toString());
-
-            Log.d("content",writer.toString());
-
-            for (String string: data){
-                writer.write(string.getBytes());
-                writer.flush();
-            }
-
-            writer.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return true;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -216,5 +123,4 @@ public class PointCloudExporter {
 
         return ret;
     }
-
 }
